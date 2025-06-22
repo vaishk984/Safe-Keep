2ace2ee02428b6fde54f16c95376778add694459
Kumari Vaishnavi <vaishnavik984@gmail.com>
2025-06-22 15:49:45 +0530

    Update README.md

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -6,7 +6,7 @@ const layout = ({ children }: { children: React.ReactNode }) => {
       <section className="hidden w-1/2 items-center justify-center bg-brand p-10 lg:flex xl:w-2/5">
         <div className="flex max-h-[800px] max-w-[430px] flex-col justify-center space-y-14">
           <Image
-            src="/logo-mobile.png"
+            src="/logo.png"
             alt="logo"
             width={300}
             height={150}

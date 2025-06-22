32ba926a8e5aa78b13b1ca3c63a5157a6ab0a9e0
Kumari Vaishnavi <vaishnavik984@gmail.com>
2025-06-22 11:19:55 +0530

    Update layout.tsx

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -7,7 +7,7 @@ import { redirect } from 'next/navigation';
 import React from 'react';
 import { Toaster } from "@/components/ui/toaster"
 
-
+export const dynamic = "force-dynamic";
 
 const Layout = async ({children} : {children: React.ReactNode}) => {
     const currentUser = await getCurrentUser();
